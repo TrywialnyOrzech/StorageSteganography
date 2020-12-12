ef48d2dea3680b77b80b7e2883267b5b85ed846d
Natan Orzechowski <natan.orze@gmail.com>
2020-12-12 18:22:05 -0500

    Send packets every n seconds feature add.

---
+++ src/execute_packets_generator.py
@@ -0,0 +1,3 @@
+from packets_generator import gen_icmp
+ip = "192.168.8.1"
+gen_icmp(ip)
---
+++ src/run_packets.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+	sudo python3.8 execute_packets_generator.py
+	sleep 5;
+done
