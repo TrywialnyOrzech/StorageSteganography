a940e3b21e3d62d21e0dff9988bc7f961b20dbcf
Natan Orzechowski <natan.orze@gmail.com>
2020-12-12 17:56:53 -0500

    Add simple packets generator.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 from src.send_icmp import icmp
+from src.sniff import sniff
+from src.packets_generator import gen_udp
+import src.test
+
 
 def main():
-    icmp()
+    #os.system('python3.8 src/test.py')
+    gen_udp("192.168.8.1")
 
 if __name__ == '__main__':
     main()
