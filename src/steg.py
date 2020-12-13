3f1ade011f1a28890542f1ce25e1ee0d1ffe96b1
Natan Orzechowski <natan.orze@gmail.com>
2020-12-13 00:53:55 -0500

    Add feature: random packet send interval.

--- src/steg.py
+++ src/steg.py
@@ -1,4 +1,6 @@
 import sys
+import time
+from random import randint
 from scapy.all import *
 from scapy.layers.inet import IP
 
@@ -7,7 +9,8 @@ def sniff_all():
 
 def steg_icmp(hex_char):
     # TODO: p = sr1(IP(chksum = 0xa9dd, dst="172.22.164.207")/ICMP())
-    p = sr1(IP(dst="192.168.8.157")/ICMP()/"ASDASDASDASD")
+    p = sr1(IP(dst="192.168.8.157")/ICMP()/hex_char)
     p.show()
+    time.sleep(randint(0, 4))
 
 
