90b2babebad4689d0007e835a86ac51fdb8e6eb4
Natan Orzechowski <natan.orze@gmail.com>
2020-12-13 00:38:27 -0500

    Read secret message from file.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from src.steg import steg_icmp
 from src.steg import sniff_all
 from src.packets_generator import gen_udp
@@ -6,8 +7,15 @@ import src.test
 
 
 def main():
+    hex_str = 'a'
+    file = open("text_to_send.txt", "r")
+    for line in file:
+        for character in line:
+            hex_char = format(ord(character), "x")
+            hex_str += hex_char
+            steg_icmp(hex_char)
     #os.system('python3.8 src/test.py')
-    steg_icmp()
+    #steg_icmp()
 
 if __name__ == '__main__':
     main()
--- src/steg.py
+++ src/steg.py
@@ -5,8 +5,9 @@ from scapy.layers.inet import IP
 def sniff_all():
     sniff(filter="icmp and host 192.168.8.155", prn=lambda x: x.show())    # lambda shows all packets being sent and received with IP headers
 
-def steg_icmp():
-    p = sr1(IP(chksum = 0xa9dd, dst="172.22.164.207")/ICMP())
+def steg_icmp(hex_char):
+    # TODO: p = sr1(IP(chksum = 0xa9dd, dst="172.22.164.207")/ICMP())
+    p = sr1(IP(dst="192.168.8.157")/ICMP()/"ASDASDASDASD")
     p.show()
 
 
